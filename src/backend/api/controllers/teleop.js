cf751f30382d978875249055f835dbb529dfab67
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-05-15 17:46:31 -0300

    feat: ROS communication :)

--- src/backend/api/controllers/teleop.js
+++ src/backend/api/controllers/teleop.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
 const config = require('config');
+const rclnodejs = require('rclnodejs');
 
 class TeleopController {
 
@@ -16,11 +17,34 @@ class TeleopController {
         ws.on('message', data => this.onMessage(ws, data));
         ws.on('error', error => this.onError(ws, error));
         ws.on('close', () => this.onClose(ws));
-        console.log('New WebSocket connection');
+        console.log('WebSocket connection established');
+
+        await rclnodejs.init();
+        const teleop_backend_node = new rclnodejs.Node('teleop_node');
+        this.linear_speed_publisher = teleop_backend_node.createPublisher('std_msgs/msg/Float32', 'linear_speed');
+        this.angular_speed_publisher = teleop_backend_node.createPublisher('std_msgs/msg/Float32', 'angular_speed');
+        this.kill_button_publisher = teleop_backend_node.createPublisher('std_msgs/msg/Bool', 'kill_button');
+        console.log('Teleop connection established');
     }
 
     async onMessage(ws, data) {
-        console.log(`Received message => ${data}`);
+        const message = JSON.parse(data);
+
+        if (message.linear_speed) {
+            console.log('Publishing linear speed:', message.linear_speed);
+            this.linear_speed_publisher.publish(message.linear_speed);
+        }
+
+        if (message.angular_speed) {
+            console.log('Publishing angular speed:', message.angular_speed);
+            this.angular_speed_publisher.publish(message.angular_speed);
+        }
+
+        if (message.kill_node) {
+            console.log('Publishing kill node:', message.kill_node);
+            this.kill_button_publisher.publish(message.kill_node);
+        }
+
         ws.send('Message received');
     }
 
