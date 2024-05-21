ac50161a2aca55c6072cf2c09ea5bbc6308c6b4e
luiza <luiza.rubim@sou.inteli.edu.br>
2024-05-21 17:57:48 -0300

    decreasing camera latency

--- src/bolin/src/bolin_camera/bolin_camera/sender.py
+++ src/bolin/src/bolin_camera/bolin_camera/sender.py
@@ -5,30 +5,45 @@ from std_msgs.msg import Header
 import cv2
 import time
 
-class WebcamPublisher(Node):
+class CameraPublisher(Node):
     def __init__(self):
-        super().__init__('webcam_publisher')
-        self.publisher_ = self.create_publisher(CompressedImage, '/video_frames', 40)
-        self.timer = self.create_timer(0.01, self.timer_callback)  # Publish every 0.1 seconds (10 Hz)
+        super().__init__('camera_publisher')
+        self.publisher_ = self.create_publisher(CompressedImage, '/camera_feed', 40)
         self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
-    def timer_callback(self):
-        ret, frame = self.cap.read()
-        if ret:
-            # Encode frame as JPEG
-            _, buffer = cv2.imencode('.jpg', frame)
-            msg = CompressedImage()
-            msg.header = Header()
-            msg.header.stamp = self.get_clock().now().to_msg()
-            msg.format = "jpeg"
-            msg.data = buffer.tobytes()
-            self.publisher_.publish(msg)
+    def spin(self):
+        try:
+            while rclpy.ok():
+                time_before = self.get_clock().now()
+                ret, frame = self.cap.read()
+                if ret:
+                    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+                    _, buffer = cv2.imencode('.jpg', frame)
+                    msg = CompressedImage()
+                    msg.header = Header()
+                    msg.header.stamp = self.get_clock().now().to_msg()
+                    msg.format = "jpeg"
+                    msg.data = buffer.tobytes()
+                    self.publisher_.publish(msg)
+                    time_after = time_before = self.get_clock().now()
+                    print(f"Latencia: {time_after - time_before} ms")
+
+                rclpy.spin_once(self, timeout_sec=0.0001)
+
+        except Exception as e:
+            self.get_logger().error(f"An error occurred: {e}")
+        
+        finally:
+            self.cap.release()
 
 def main(args=None):
     rclpy.init(args=args)
-    webcam_publisher = WebcamPublisher()
-    rclpy.spin(webcam_publisher)
-    webcam_publisher.destroy_node()
+    camera_publisher = CameraPublisher()
+    camera_publisher.spin()
+    camera_publisher.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
