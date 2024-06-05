0a9a7a0ef507ed08d807001cbc7209e9d209de35
ItsVasconcellos <fernando.asv@uol.com.br>
2024-06-05 17:28:53 -0300

    feat: crud - reboiler - controller & routes

--- src/backend/config/express.js
+++ src/backend/config/express.js
@@ -8,14 +8,9 @@ const morgan = require('morgan');
 // Api Routes
 const robotRoutes = require('../api/routes/robot');
 const unityRoutes = require('../api/routes/unity');
-
-/*
 const reboilerRoutes = require('../api/routes/reboiler');
-const imageRoutes = require('../api/routes/image');
-*/
-
+//const imageRoutes = require('../api/routes/image');
 const tubeRoutes = require('../api/routes/tube');
-
 /*
 const examinationRoutes = require('../api/routes/examination');
 const tubeStateRoutes = require('../api/routes/tubeState'); 
@@ -38,8 +33,8 @@ module.exports = () => {
     // Api
     app.use('/api/robots', robotRoutes);
     app.use('/api/unities', unityRoutes);
-    /*app.use('/api/reboilers', reboilerRoutes);
-    app.use('/api/images', imageRoutes);*/
+    app.use('/api/reboilers', reboilerRoutes);
+    //app.use('/api/images', imageRoutes);
     app.use('/api/tubes', tubeRoutes);
     /*app.use('/api/examinations', examinationRoutes);
     app.use('/api/tube-states', tubeStateRoutes);*/
