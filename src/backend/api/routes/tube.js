f340d415f154f16c1373b406ed5f224edd319fb1
ItsVasconcellos <fernando.asv@uol.com.br>
2024-06-08 21:53:19 -0300

    fix: fixing routes of tube.js

--- src/backend/api/routes/tube.js
+++ src/backend/api/routes/tube.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const tubeController = require('../controllers/tube');
 
-router.get('/tubes', tubeController.getAllTubes);
-router.get('/tubes/:id', tubeController.getTubeById);
-router.post('/tubes', tubeController.createTube);
-router.put('/tubes/:id', tubeController.updateTube);
-router.delete('/tubes/:id', tubeController.deleteTube);
+router.get('/', tubeController.getAllTubes);
+router.get('/:id', tubeController.getTubeById);
+router.post('/', tubeController.createTube);
+router.put('/:id', tubeController.updateTube);
+router.delete('/:id', tubeController.deleteTube);
  
 module.exports = router;
