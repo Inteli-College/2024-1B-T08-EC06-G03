e56d822ffee5b35014425a2ae4357506e77d405e
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-06-12 17:34:02 -0300

    fix: websocket url to work in other devices

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -47,7 +47,7 @@ const App: React.FC = () => {
 
     const startCamera = () => {
         const ros = new ROSLIB.Ros({
-            url: 'ws://localhost:9090'
+            url: `ws://${window.location.hostname}:9090`
         });
 
         ros.on('connection', () => {
