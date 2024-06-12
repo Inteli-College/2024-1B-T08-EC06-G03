88a7ae05718c98935cd0996ec46396d31047b233
ifferreira <ivan.ferreira@sou.inteli.edu.br>
2024-06-12 17:10:56 -0300

    fix: database sql syntax

--- src/backend/scripts/createDatabase.sql
+++ src/backend/scripts/createDatabase.sql
@@ -1,63 +1,63 @@
 -- Create the Robot table
-CREATE TABLE IF NOT EXISTS Robot (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  last_manufactured text NOT NULL
+CREATE TABLE IF NOT EXISTS `Robot` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `last_manufactured` TEXT NOT NULL
 );
 
 -- Create the Unit table
-CREATE TABLE IF NOT EXISTS Unit (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  city text,
-  state text
+CREATE TABLE IF NOT EXISTS `Unit` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `city` TEXT,
+  `state` TEXT
 );
 
 -- Create the Reboiler table
-CREATE TABLE IF NOT EXISTS Reboiler (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  number int NOT NULL,
-  unit_id int,
-  started_at text,
-  finished_at text,
-  FOREIGN KEY (unit_id) REFERENCES Unit(id)
+CREATE TABLE IF NOT EXISTS `Reboiler` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `number` INTEGER NOT NULL,
+  `unit_id` INTEGER,
+  `started_at` TEXT,
+  `finished_at` TEXT,
+  FOREIGN KEY (`unit_id`) REFERENCES `Unit`(`id`)
 );
 
 -- Create the Order table
-CREATE TABLE IF NOT EXISTS Order (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  status text,
-  robot_id int,
-  reboiler_id int,
-  started_at text,
-  finished_at text,
-  FOREIGN KEY (robot_id) REFERENCES Robot(id),
-  FOREIGN KEY (reboiler_id) REFERENCES Reboiler(id)
+CREATE TABLE IF NOT EXISTS `Order` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `status` TEXT,
+  `robot_id` INTEGER,
+  `reboiler_id` INTEGER,
+  `started_at` TEXT,
+  `finished_at` TEXT,
+  FOREIGN KEY (`robot_id`) REFERENCES `Robot`(`id`),
+  FOREIGN KEY (`reboiler_id`) REFERENCES `Reboiler`(`id`)
 );
 
 -- Create the Image table
-CREATE TABLE IF NOT EXISTS Image (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  image text NOT NULL,
-  taken_at text NOT NULL
+CREATE TABLE IF NOT EXISTS `Image` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `image` TEXT NOT NULL,
+  `taken_at` TEXT NOT NULL
 );
 
 -- Create the Examination table
-CREATE TABLE IF NOT EXISTS Examination (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  step text NOT NULL,
-  started_at text NOT NULL,
-  finished_at text NOT NULL,
-  order_id int,
-  FOREIGN KEY (order_id) REFERENCES Order(id)
+CREATE TABLE IF NOT EXISTS `Examination` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `step` TEXT NOT NULL,
+  `started_at` TEXT NOT NULL,
+  `finished_at` TEXT NOT NULL,
+  `order_id` INTEGER,
+  FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`)
 );
 
 -- Create the TubeState table
-CREATE TABLE IF NOT EXISTS TubeState (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  dirtness bool NOT NULL,
-  image_id int,
-  examination_id int,
-  reboiler_id int,
-  FOREIGN KEY (image_id) REFERENCES Image(id),
-  FOREIGN KEY (examination_id) REFERENCES Examination(id),
-  FOREIGN KEY (reboiler_id) REFERENCES Tube(id)
+CREATE TABLE IF NOT EXISTS `TubeState` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `dirtness` BOOLEAN NOT NULL,
+  `image_id` INTEGER,
+  `examination_id` INTEGER,
+  `reboiler_id` INTEGER,
+  FOREIGN KEY (`image_id`) REFERENCES `Image`(`id`),
+  FOREIGN KEY (`examination_id`) REFERENCES `Examination`(`id`),
+  FOREIGN KEY (`reboiler_id`) REFERENCES `Reboiler`(`id`)
 );
