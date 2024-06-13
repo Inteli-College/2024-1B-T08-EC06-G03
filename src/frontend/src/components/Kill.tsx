6edde283777849b416c3066f1f6b8d6df66fd105
GuilhermeLinhares04 <guilherme.linhares@sou.inteli.edu.br>
2024-06-13 18:00:23 -0300

    feat: added toast when robot is killed

--- src/frontend/src/components/Kill.tsx
+++ src/frontend/src/components/Kill.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import '../styles/kill.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface KillButtonProps {
     sendMessage: (message: string) => void;
 }
 
 const KillButton: React.FC<KillButtonProps> = ({ sendMessage }) => {
+    const notify = () => toast.error("O botão de matar processo foi acionado");
     const handleClick = () => {
         sendMessage(JSON.stringify({ kill_robot: 'kill' }));
     };
 
     return (
-        <button
-            className="bg-red-500 text-2xl text-white font-bold rounded-full flex items-center justify-center transition-transform transform hover:bg-red-800"
-            style={{ width: '7.5rem', height: '7.5rem' }} // 3/4 of 10rem (joystick's diameter is 10rem)
-            onClick={handleClick}
-        >
-            KILL
-        </button>
+        <div>
+            <button
+                className="bg-red-500 text-2xl text-white font-bold rounded-full flex items-center justify-center transition-transform transform hover:bg-red-800"
+                style={{ width: '7.5rem', height: '7.5rem' }} // 3/4 of 10rem (joystick's diameter is 10rem)
+                onClick={() => {
+                    handleClick();
+                    notify();
+                }}
+            >
+                KILL
+            </button>
+            <ToastContainer 
+                position="bottom-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+                />
+            {/* Same as */}
+        </div>
+
     );
 };
 
