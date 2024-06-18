4e6bdb3c3e7dd6a19063d96da8495d5bcae76940
ifferreira <ivan.ferreira@sou.inteli.edu.br>
2024-06-18 18:07:27 -0300

    fix: route returns base64 properly

--- src/model/app.py
+++ src/model/app.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import base64
 
-model = YOLO("oloy.pt")
+model = YOLO("oloy_verdadeiro.pt")
 app = Flask(__name__)
 
 @app.route("/infer", methods=["POST"])
@@ -13,22 +13,19 @@ def infer():
     data = request.json
     base64_img = data.get("base64_img")
     resultado = inferencer(base64_img)
-    #resultado= decode_string(base64_img)
     return resultado
-
-def decode_string(string):
-    new_string = base64.b64decode(str(string))
-    return new_string
 
 def inferencer(base64_img):
     try:
         img = base64.b64decode(str(base64_img))
+        new_img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
     except Exception as e:
         return f"Erro na decodificação da imagem base64: {str(e)}"
 
-    results = model.predict(base64_image=img)
+    results = model.predict(new_img)
+
     for result in results:
-        annotator = Annotator(img)
+        annotator = Annotator(new_img)
         boxes = result.boxes
 
         for box in boxes:
@@ -37,9 +34,10 @@ def inferencer(base64_img):
             annotator.box_label(b, model.names[int(c)])
 
     annotated_image = annotator.result()
+    _, buffer = cv2.imencode(".jpg", annotated_image)
     inferenced_image = annotated_image.copy()
-    base64_inferenced_img = base64.b64encode(inferenced_image)
-    return base64_inferenced_img
+    base64_inferenced_image = base64.b64encode(buffer).decode("utf-8")
+    return base64_inferenced_image
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
