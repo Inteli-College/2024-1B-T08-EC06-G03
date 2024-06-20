a65183f146984e13be1f263ef2a89fd40378af4f
ItsVasconcellos <fernando.asv@uol.com.br>
2024-06-20 10:57:54 -0300

    feat: resolve multiples fetch

--- src/frontend/src/api/orders.ts
+++ src/frontend/src/api/orders.ts
@@ -1,36 +1,20 @@
 
 import { Examination, Order } from '../components/Columns';
 
-export const getOrders = async (): Promise<Order[] | string> => {
+export const getOrders = async (unit_id: number): Promise<Order[] | string> => {
     try {
-      const [ordersResponse, examinationsResponse] = await Promise.all([
-        fetch('http://localhost:8000/api/orders', {
+      const ordersResponse= await fetch(`http://localhost:8000/api/orders/unit/${unit_id}`, {
           method: 'GET',
           headers: {
             'Cache-Control': 'no-cache',
           },
-        }),
-        fetch('http://localhost:8000/api/examinations', {
-          method: 'GET',
-          headers: {
-            'Cache-Control': 'no-cache',
-          },
-        }),
-      ]);
-
-      if (!ordersResponse.ok || !examinationsResponse.ok) {
+      });
+      if (!ordersResponse.ok) {
         throw new Error('Failed to fetch data from one or both endpoints');
       }
-
-      const orders = await ordersResponse.json();
-      const examinations = await examinationsResponse.json();
-
-      return orders.map((order: Order) => ({
-        ...order,
-        examinations: examinations.filter((examination: Examination) => examination.order_id === order.id),
-      }));
-
-    } catch (error) {
+      return ordersResponse.json() 
+    }
+    catch (error) {
       return error instanceof Error ? error.message : String(error);
     }
   }
