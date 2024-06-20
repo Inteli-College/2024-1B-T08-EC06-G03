70b930571d1d53f022713dc00cfaae93863fb8eb
ItsVasconcellos <fernando.asv@uol.com.br>
2024-06-20 11:21:03 -0300

    feat: adding logic to deal with get

--- src/frontend/src/components/UnitDropdown.tsx
+++ src/frontend/src/components/UnitDropdown.tsx
@@ -8,21 +8,26 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { getUnities } from '@/api/unit';
+import { Unit } from '@/components/Columns';
 
 export function UnitDropdown() {
-  const [options, setOptions] = React.useState([]);
+  const [options, setOptions] = React.useState<Unit[] | [] >([]);
+  const [error, setError] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     async function getOptions() {
-      try {
-        const response = await fetch('https://api.example.com/units'); // Replace with your actual endpoint
-        const data = await response.json();
+    try{
+      const data: Unit[] | string   = await getUnities();
+      if (typeof data === "string") {
+        setError(data);
+      } else {
         setOptions(data);
-      } catch (error) {
-        console.error('Error fetching options:', error);
       }
-    }
-
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError('An error occurred while fetching data.'); 
+    }  finally{}}
     getOptions();
   }, []);
 
