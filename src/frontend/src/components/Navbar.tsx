e0008fa24fa5b4528615dad603d840c6be75cdd3
luiza <luiza.rubim@sou.inteli.edu.br>
2024-06-20 23:15:36 -0300

    fix: change navbar name

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -5,8 +5,9 @@ const Navbar: React.FC = () => {
     <nav className="bg-green-800 p-2 fixed w-full top-0 z-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
-          <div className="flex-shrink-0">
-            <h1 className="text-white text-2xl font-bold">Rebólins</h1>
+          <div className="flex flex-row items-center">
+            <img className="h-10 w-10" src="/logo2.png" alt="Workflow" />
+            <h1 className="text-white text-2xl font-bold">Desencana!</h1>
           </div>
         </div>
       </div>
